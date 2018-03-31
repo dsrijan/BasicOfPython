33e25e39e2aa0f17fdd601f1b93ec839bde041b6
dsrijan <srijan12345@gmail.com>
2018-04-01 01:12:53 +0530

    Basics Of Python

---
+++ helloWold.py
@@ -0,0 +1,63 @@
+"""
+this is comment
+
+"""
+
+
+def srijanCalled():
+    print("srijan calling a function")
+
+srijanCalled()
+
+
+def userName(name):
+    return("user name is " + name)
+
+save = userName("srijan")
+print(save)
+
+a = 5
+b = 6
+print(a + b)
+
+c = "srijan"
+
+print(c +  " " + str(b))
+
+sent = "%s %s is the president of America"
+print(sent%("Barack","Obama"))
+
+age = "My age is %d years %s old"
+print(age%(5,"srijan"))
+
+shopList = ["Apple","Orange","Grape","Litchi"]
+print(shopList[3])
+print(len(shopList))
+
+students = {"bob":13, "marley": 15, "santa": 19, "banta": 22}
+print(len(students))
+
+
+if (4 < 3):
+    print("srijan")
+else:
+    print("hello")
+
+list1 = [1,2,3,4,5,6]
+for i in list1 :
+    print(i)
+
+for a in range(1, 10, 2):
+    print(a)
+
+try:
+    if srijan < 3:
+        print("error bro")
+except:
+    print("catched error")
+
+a = "Srijan"
+
+print(a)
+
+# this is a comment
---
+++ object.py
@@ -0,0 +1,30 @@
+
+
+
+
+class Person:
+    name = "srijan"
+    age = 26
+
+    def __init__(self, name):
+        self.name = name
+        pass
+
+    def isHeMale():
+        return true
+
+p = Person("srijankumar")
+# p = Person()
+q = Person("amitpathak")
+print(p.age)
+p.age = 222
+q.age = p.age
+
+print(p.name)
+print(q.age)
+
+
+def sumTwoNumber(num1, num2):
+    print(num1 + num2)
+
+sumTwoNumber(2, 4)
